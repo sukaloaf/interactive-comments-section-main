8693902de7face3985102b3516a73e6dcdf42206
Seth Brown <sdbrown93@gmail.com>
2023-10-13 16:45:04 -0400

    menu updates

--- scripts/avatarMenu.js
+++ scripts/avatarMenu.js
@@ -7,3 +7,62 @@ const defaultNavElements = `
         <span class="lines line-2"></span>
     </label>
 `;
+const menu = document.querySelector(".menu-contents");
+
+// Menu toggle
+menu.style.display = "none";
+
+function displayToggle() {
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
+navMenu.addEventListener("click", () => {
+  displayToggle();
+});
+
+// Choose avatar
+function createMenuAvatarElement(name) {
+  const avatar = document.createElement("a");
+  avatar.href = "#";
+  avatar.classList.add("menu-item");
+  avatar.id = name;
+  avatar.innerHTML = `
+        <picture>
+            <source srcset="../images/avatars/image-${name}.webp" type="image/webp">
+            <img class="user-avatar menu-avatar" src="../images/avatars/image-${name}.png" alt="User Avatar">
+        </picture>
+    `;
+
+  avatar.addEventListener("click", () => {
+    // Remove the clicked username from the array
+    const index = usernameArray.indexOf(name);
+    if (index > -1) {
+      usernameArray.splice(index, 1);
+    }
+
+    // Add the clicked username to the beginning of the array
+    usernameArray.unshift(name);
+
+    // Empty all avatar elements in navMenu
+    navMenu.innerHTML = defaultNavElements;
+
+    // Re-create the avatars in the updated order
+    usernameArray.forEach((username) => {
+      navMenu.appendChild(createMenuAvatarElement(username));
+    });
+  });
+
+  return avatar;
+}
+
+window.addEventListener("load", () => {
+  navMenu.innerHTML = defaultNavElements;
+
+  usernameArray.forEach((username) => {
+    navMenu.appendChild(createMenuAvatarElement(username));
+  });
+});
--- scripts/colorTheme.js
+++ scripts/colorTheme.js
@@ -1,7 +1,7 @@
 /* This js script is for the toggle switch that changed the theme */
 function toggleTheme() {
   document.getElementById("toggle").addEventListener("click", () => {
-    document.getElementsByTagName("body")[0].classList.toggle("light-mode");
+    document.getElementsByTagName("body")[0].classList.toggle("dark-mode");
   });
 }
 
