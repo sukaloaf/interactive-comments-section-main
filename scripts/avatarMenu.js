476986891b12d2300d50eef16b94f6ad06cc9a4d
Seth Brown <sdbrown93@gmail.com>
2023-10-13 21:01:02 -0400

    menu updates

--- scripts/avatarMenu.js
+++ scripts/avatarMenu.js
@@ -1,13 +1,8 @@
 const navMenu = document.querySelector(".menu");
 const usernameArray = ["maxblagun", "amyrobson", "ramsesmiron", "juliusomo"];
-const defaultNavElements = `
-    <input type="checkbox" class="menu-open" name="menu-open" id="menu-open" />
-    <label class="menu-open-button" for="menu-open">
-        <span class="lines line-1"></span>
-        <span class="lines line-2"></span>
-    </label>
-`;
+const defaultNavElements = ``;
 const menu = document.querySelector(".menu-contents");
+const avatarIcons = document.querySelector(".avatar-icons");
 
 // Menu toggle
 menu.style.display = "none";
@@ -35,34 +30,16 @@ function createMenuAvatarElement(name) {
             <source srcset="../images/avatars/image-${name}.webp" type="image/webp">
             <img class="user-avatar menu-avatar" src="../images/avatars/image-${name}.png" alt="User Avatar">
         </picture>
+        <p>${name}</p>
     `;
-
-  avatar.addEventListener("click", () => {
-    // Remove the clicked username from the array
-    const index = usernameArray.indexOf(name);
-    if (index > -1) {
-      usernameArray.splice(index, 1);
-    }
-
-    // Add the clicked username to the beginning of the array
-    usernameArray.unshift(name);
-
-    // Empty all avatar elements in navMenu
-    navMenu.innerHTML = defaultNavElements;
-
-    // Re-create the avatars in the updated order
-    usernameArray.forEach((username) => {
-      navMenu.appendChild(createMenuAvatarElement(username));
-    });
-  });
 
   return avatar;
 }
 
 window.addEventListener("load", () => {
-  navMenu.innerHTML = defaultNavElements;
+  avatarIcons.innerHTML = defaultNavElements;
 
   usernameArray.forEach((username) => {
-    navMenu.appendChild(createMenuAvatarElement(username));
+    avatarIcons.appendChild(createMenuAvatarElement(username));
   });
 });
